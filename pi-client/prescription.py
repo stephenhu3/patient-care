e4e79552d9ce41b731d4f4cd05c55cf947e9a694
aSwains <swainsonalexander@gmail.com>
2015-11-20 10:03:49 -0800

    Update prescription class for new database relations

--- pi-client/prescription.py
+++ pi-client/prescription.py
@@ -8,8 +8,10 @@ import time
 import datetime
 
 class PiPrescription(object):
-	def __init__(self, alerts, medication_name, instructions):
-		self.alerts = alerts
+	def __init__(self, alertsForToday, medication_name, instructions, timeout):
+		self.alertsForToday = alertsForToday
 		self.medication_name = medication_name
 		self.instructions = instructions
-		self.triggered = [0] * len(alerts)
+		self.triggered = [0] * len(alertsForToday)
+		self.timeout = timeout
+		self.dayOfWeek = datetime.date.today().strftime("%A")

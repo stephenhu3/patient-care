e9a31a88c0db2254595403dc830247c4eb57741b
stephenhu3 <stephenhu3@gmail.com>
2015-11-21 15:05:36 -0800

    added access control headers for patient care web HTTPS url

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ var uriUtil    = require('mongodb-uri');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://patient-care-web.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Fill in credentials
 var DBUSER = process.env.MONGOLAB_USER;
 var DBPASS = process.env.MONGOLAB_PASS;
